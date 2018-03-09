805e68caeae18233100db21b8ad2c55a19bbcd9d
Cody Kinneer <ckinneer@cs.cmu.edu>
2018-03-08 23:55:36 -0500

    copied over harsanyi transform

--- gen_sum.py
+++ gen_sum.py
@@ -43,8 +43,11 @@ def expected_time_in_system(a, d):
 def adjusted_system_time(a, d, t):
     techniqueFit = 1
 
-    if (a != t):
-        techniqueFit *= 0.5;
+    if a != t:
+        if t == 1:
+            techniqueFit *= 0.5
+        elif t == 2:
+            techniqueFit *= 0.5
 
     return expected_time_in_system(a, d) * techniqueFit
 
@@ -86,5 +89,28 @@ for a in range(strategies_p1):
 
 s = gambit.nash.ExternalEnumMixedSolver()
 
+
+print(s.solve(g))
+
+strategies_p1 = 4
+strategies_p2 = 4
+
+g = gambit.Game.new_table([strategies_p1, strategies_p2])
+
+T1_PRIOR = 0.5
+T2_PRIOR = 0.5
+
+a = 0
+for a1 in range(2):
+    for a2 in range(2):
+        for d in range(strategies_p2):
+            pa = T1_PRIOR * payoff_attacker(a1+1, d+1, 1) + T2_PRIOR * payoff_attacker(a2+1, d+1, 2)
+            pd = T1_PRIOR * payoff_defender(a1+1, d+1, 1) + T2_PRIOR * payoff_defender(a2+1, d+1, 2)
+            g[a, d][0] = Decimal(pa)
+            g[a, d][1] = Decimal(pd)
+        a += 1
+
+g
+s = gambit.nash.ExternalEnumMixedSolver()
 
 print(s.solve(g))
