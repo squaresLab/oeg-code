834e30cfa44311fc92da5bc967f4140aa83a0d7f
Cody Kinneer <ckinneer@cs.cmu.edu>
2018-04-11 16:58:03 -0400

    generate graphs for stackelberg sweep

--- sweep_analysis.r
+++ sweep_analysis.r
@@ -3,12 +3,25 @@ library(reshape)
 library(ggplot2)
 
 nashsweep <- read_csv("PycharmProjects/apt/nashsweep.csv")
-mdata <- melt(nashsweep,id=c("t1_prior","payoff"))
-ggplot(mdata, aes(variable,t1_prior, fill = value)) + geom_raster()
+
+bothsweep <- read_csv("~/PycharmProjects/apt-code/bothsweep.csv")
+
+sweep <- bothsweep
+
+sweeps <- subset(sweep,equilibrium == "nash")
+title <- "Nash"
+
+sweeps <- subset(sweep,equilibrium != "nash")
+title <- "Stackelberg"
+
+sweeps <- as.data.frame(sweeps)
+
+mdata <- melt(sweeps,id=c("t1_prior","payoff","equilibrium"))
+ggplot(mdata, aes(variable,t1_prior, fill = value)) + geom_raster() + ggtitle(title)
 
 attacker = subset(mdata,variable %in% c("11","12","21","22"))
 defender = subset(mdata,!variable %in% c("11","12","21","22"))
-ggplot(attacker, aes(variable,t1_prior, fill = value)) + geom_raster()
-ggplot(defender, aes(variable,t1_prior, fill = value)) + geom_raster()
+ggplot(attacker, aes(variable,t1_prior, fill = value)) + geom_raster() + ggtitle(title)
+ggplot(defender, aes(variable,t1_prior, fill = value)) + geom_raster() + ggtitle(title)
 
-(p <- ggplot(mdata, aes(variable, t1_prior)) + geom_tile(aes(fill = value), colour = "white") + scale_fill_gradient(low = "white",high = "steelblue"))
+(p <- ggplot(mdata, aes(variable, t1_prior)) + geom_tile(aes(fill = value), colour = "white") + scale_fill_gradient(low = "white",high = "steelblue") + ggtitle(title))
