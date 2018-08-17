bf938ec629b20e2a995237aab01893ac141ebfcf
Cody Kinneer <ckinneer@cs.cmu.edu>
2018-08-16 21:14:21 -0400

    timestep sweep now running

--- timestep_sweep.py
+++ timestep_sweep.py
@@ -1,7 +1,7 @@
 from gurobi_stackelberg_equilibrium_solver_extended import get_stackelberg, get_attacker_br
 from harsanyi_nash_equilibrium_solver_extended import get_solution, strategies_p1, strategies_p2, getMixedStrategyProfile
 
-print("t1_prior,equilibrium,11,12,21,22,we1,we2,e1,e2,payoff,uniform")
+print("timestep,equilibrium,11,12,21,22,we1,we2,e1,e2,payoff,uniform")
 
 for x in range(101):
     t = x + 1
@@ -15,25 +15,41 @@ for x in range(101):
 
     linen = str(t) + ",nash,"
     lines = str(t) + ",stackelberg,"
-    for x in range(strategies_p1 + strategies_p2):
+
+    strategies_p1 = 3**3 # attacker
+    strategies_p2 = 3 * t # defender
+
+    stackelberg_length = 3 * 3 + strategies_p2
+    nash_length = strategies_p1 + strategies_p2
+
+    for x in range(nash_length):
         linen += str(solution[x]) + ","
-        s = x
-        if 0 <= s < 4:
-            s += 4
+        if x < strategies_p1: # if we are in attacker, copy attackers nash action for computing defenders loss if playing uniform random
             random_profile[x] = solution[x]
-        elif 3 < s < 8:
-            s -= 4
-        lines += str(ans[s]) + ","
-
     linen += str(solution.payoff(1)) + ","
-    lines += str(ans[strategies_p1 + strategies_p2]) + ","
 
-    br_to_random = get_attacker_br([.25, .25, .25, .25], timesteps=t, debug=False)
+    for x in range(stackelberg_length):
+        # since we want attacker to go first, add an offset for the first 3*3 x values
+        if 0 <= x < 3*3:
+            lines += str(ans[x+strategies_p2]) + ","
+        else:
+            lines += str(ans[x-3*3]) + ","
+
+    lines += str(ans[3*3 + strategies_p2]) + ","
+
+    dp = []
+
+    uniform = 1.0 / strategies_p2
+
+    for x in range(strategies_p2):
+        dp.append(uniform)
+
+    br_to_random = get_attacker_br(dp, timesteps=t, debug=False)
     rand = [v.x for v in br_to_random.getVars()]
     rand.append(br_to_random.objVal)
 
     linen += str(random_profile.payoff(1))
-    lines += str(rand[strategies_p1 + strategies_p2])
+    lines += str(rand[3+3 + strategies_p2])
 
     print(linen)
     print(lines)
