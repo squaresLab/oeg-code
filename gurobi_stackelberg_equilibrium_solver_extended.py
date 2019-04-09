7a8f22e994e70fcf58a869637be99c28391d0f52
Cody Kinneer <ckinneer@cs.cmu.edu>
2019-04-09 15:45:05 -0400

    new solver functions to accept model objects

--- gurobi_stackelberg_equilibrium_solver_extended.py
+++ gurobi_stackelberg_equilibrium_solver_extended.py
@@ -71,6 +71,67 @@ def get_attacker_br(dp, timesteps=2, p_t1=0.33, p_t2=0.33, p_tn=0.33,debug=True,
         print('Error reported')
 
 
+def get_stackelberg_from_model(model, timesteps=2, t1_prior=0.33, t2_prior=0.33, tn_prior=0.33, debug=False):
+    try:
+        num_attackers = 2
+        num_ttps = 2
+
+        strategies_p1 = num_ttps**num_attackers
+        # strategies_p2 = timesteps*num_ttps
+        strategies_p2 = ((timesteps ** 2 + timesteps) // 2) * (num_attackers+1) # plus one for pass tactic case
+
+        nation_prior = t1_prior
+        criminal_prior = t2_prior
+        no_attacker_prior = tn_prior
+
+        # Create a new model
+        m = Model("mip1")
+
+        m.setParam('OutputFlag', debug)
+
+        # Create variables
+        # defenders actions
+        p = m.addVars(strategies_p2, vtype=GRB.CONTINUOUS)
+        # attackers actions
+        a = m.addVars(num_attackers, num_ttps, vtype=GRB.BINARY)
+
+        # Set objective
+        obj = LinExpr()
+
+        for d in range(strategies_p2):
+            for a_index in range(num_ttps):
+                obj += nation_prior * model.payoff_defender_single_defender_arg(a_index+1, d, 1) * p[d] * a[0, a_index]
+                # print("added "+str(nation_prior)+" * "+str(model.payoff_defender_single_defender_arg(a_index+1, d, 1))+" * "+str(p[d])+" * "+str(a[0, a_index]))
+                obj += criminal_prior * model.payoff_defender_single_defender_arg(a_index+1, d, 2) * p[d] * a[1, a_index]
+                # print("added " + str(criminal_prior) + " * " + str(model.payoff_defender_single_defender_arg(a_index + 1, d, 2)) + " * " + str(p[d]) + " * " + str(a[1, a_index]))
+            obj += no_attacker_prior * model.payoff_defender_single_defender_arg(1, d, 3) * p[d]
+            # print("added " + str(no_attacker_prior) + " * " + str(
+            #     model.payoff_defender_single_defender_arg(a_index + 1, d, 3)) + " * " + str(p[d]))
+
+        obj *= -1
+        m.setObjective(obj, GRB.MINIMIZE)
+
+        # Add constraints
+        # defenders ps sum to 1
+        m.addConstr(p.sum() == 1.0)
+        # each attacker chooses one pure strategy
+        m.addConstrs(a.sum(i, '*') == 1 for i in range(num_attackers))
+        # each attacker type best responds to the defenders choices
+        # build payoff values for each attacker type and attacker choice
+        coeff = {(t, action): p.prod({d: model.payoff_attacker_single_defender_arg(action+1, d, t+1) for d in range(strategies_p2)})
+                 for action in range(num_ttps) for t in range(num_attackers)}
+
+        # for each possible action, check that the chosen action is >= (i.e., that the attacker best responds)
+        m.addConstrs(a.prod(coeff, t, '*') >= coeff[(t, action)]
+                     for action in range(num_ttps) for t in range(num_attackers))
+
+        m.optimize()
+
+        return m
+    except GurobiError:
+        print('Error reported')
+
+
 def get_stackelberg(timesteps=2, t1_prior=0.33, t2_prior=0.33, tn_prior=0.33, debug=False, ttp1_obs=0.1, ttp2_obs=0.9):
     model = ModelExtendedGen(ttp1_obs, ttp2_obs, horizon=timesteps)
 
--- harsanyi_nash_equilibrium_solver_extended.py
+++ harsanyi_nash_equilibrium_solver_extended.py
@@ -4,6 +4,38 @@ from decimal import *
 
 from extended_model_gen import ModelExtendedGen
 
+
+def get_solution_from_model(model, timesteps=2, p_t1=0.33, p_t2=0.33, p_na=0.33, num_attackers=2, num_ttps=2):
+    strategies_p1 = num_ttps ** num_attackers
+    strategies_p2 = ((timesteps ** 2 + timesteps) // 2) * (num_attackers + 1)  # num_attackers+1 is nessissary for handleing pass tactic case
+
+    g = gambit.Game.new_table([strategies_p1, strategies_p2])
+
+    g.title = "APT Observability"
+
+    g.players[0].label = "Attacker"
+    g.players[1].label = "Defender"
+
+    a = 0
+    for a1 in range(2):
+        for a2 in range(2):
+            for d in range(strategies_p2):
+                pa = p_t1 * model.payoff_attacker_single_defender_arg(a1 + 1, d,
+                                                                          1) + p_t2 * model.payoff_attacker_single_defender_arg(
+                    a2 + 1, d, 2) + p_na * model.payoff_attacker_single_defender_arg(a2 + 1, d, 3)
+                pd = p_t1 * model.payoff_defender_single_defender_arg(a1 + 1, d,
+                                                                          1) + p_t2 * model.payoff_defender_single_defender_arg(
+                    a2 + 1, d, 2) + p_na * model.payoff_defender_single_defender_arg(a2 + 1, d, 3)
+                # print(str(pa) + " " + str(pd))
+                g[a, d][0] = Decimal(pa)
+                g[a, d][1] = Decimal(pd)
+            a += 1
+
+    s = gambit.nash.ExternalEnumMixedSolver()
+
+    solution = s.solve(g)
+    return solution
+
 def get_solution(timesteps=2, p_t1=0.33, p_t2=0.33, p_na=0.33, ttp1_obs=0.1, ttp2_obs=0.9):
 
     strategies_p2 = 3 * timesteps
@@ -86,7 +118,7 @@ def getMixedStrategyProfile(timesteps=2, p_t1=0.33, p_t2=0.33, p_na=0.33, ttp1_o
 
 
 def main():
-    timesteps=2
+    timesteps=50
     num_attackers = 2
     num_ttps = 2
 
@@ -94,7 +126,7 @@ def main():
     # strategies_p2 = timesteps*num_ttps
     strategies_p2 = ((timesteps ** 2 + timesteps) // 2) * (num_attackers + 1)  # plus one for pass tactic case
 
-    solution = get_solution(timesteps=timesteps, p_t1=0.5, p_t2=0.5, p_na=0)
+    solution = get_solution(timesteps=timesteps, p_t1=0.33, p_t2=0.33, p_na=0.33)
 
     if (len(solution)==1):
         solution = solution[0]
