9ce55f856518ef7d591b878f57c0975b23b0bc28
Cody Kinneer <ckinneer@cs.cmu.edu>
2019-05-28 18:28:36 -0400

    small changes to preproc script

--- interp_preproc.py
+++ interp_preproc.py
@@ -29,39 +29,47 @@ def parse_defender_actions(d):
     return wait, blind_evict, active_place
 
 header = "timesteps,q(TTP1),q(TTP2),a(11),a(12),a(21),a(22),a(n1),a(n2),p(1),p(2),p(3),1,2,3,d(1),d(2),d(3),c(E1),c(E2),c(P),c(A),t1,t2,na,equilibrium,num_eqs,11,12,21,22,e1,e2,p,we1,we2,wp,ae1,ae2,ap,payoff"
+post_proc_header = "timesteps,q(TTP1),q(TTP2),a(11),a(12),a(21),a(22),a(n1),a(n2),p(1),p(2),p(3),1,2,3,d(1),d(2),d(3),c(E1),c(E2),c(P),c(A),t1,t2,na,equilibrium,num_eqs,11,12,21,22,wait,active,pass,payoff"
+
+print(post_proc_header)
 
 non_actions = 28
 attacker_actions = 4
 
-filename = "test.csv"
+# filename = "/home/ckinneer/PycharmProjects/apt-code/interp_results/out.nash.merged.csv"
+filename = "/home/ckinneer/PycharmProjects/apt-code/interp_results/out.stackelberg.0.csv"
+
 with open(filename) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=",")
     line_count = 0
     for row in csv_reader:
         defender_actions = len(row) - non_actions - attacker_actions
-        if line_count == 0:
+        if line_count == -1:
             print(", ".join(row))
             line_count += 1
         else:
-            rowsize = len(row)
-            outline = row[0:non_actions-1+attacker_actions]
+            try:
+                rowsize = len(row)
+                outline = row[0:non_actions-1+attacker_actions]
 
-            defender = row[non_actions-1+attacker_actions:-1]
+                defender = row[non_actions-1+attacker_actions:-1]
 
-            wait = active = pas = 0
-            for action in range(len(defender)):
-                parsed = parse_defender_actions(action)
-                wait += parsed[0] * float(defender[action])
-                if parsed[2] > -1:
-                    active += float(defender[action])
-                if parsed[1] == 3:
-                    pas += float(defender[action])
+                wait = active = pas = 0
+                for action in range(len(defender)):
+                    parsed = parse_defender_actions(action)
+                    wait += parsed[0] * float(defender[action])
+                    if parsed[2] > -1:
+                        active += float(defender[action])
+                    if parsed[1] == 3:
+                        pas += float(defender[action])
 
-            outline.append(str(wait))
-            outline.append(str(active))
-            outline.append(str(pas))
+                outline.append(str(wait))
+                outline.append(str(active))
+                outline.append(str(pas))
 
-            outline.append(row[-1])
+                outline.append(row[-1])
 
-            print(",".join(outline))
-            print(str(len(outline)))
+                print(",".join(outline))
+            except ValueError:
+                print("WARNING ERROR! SKIPPED ROW: "+", ".join(row))
+
